35350ba28abbde054b2a5a296d8d5c98d79d8122
Liudas Sodonis <liudas@sodonis.lt>
2015-12-03 21:45:42 -0800

    Added views, basic functionality works

--- links/shorts/urls.py
+++ links/shorts/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<short>[a-z0-9]+)/$', views.short, name='short'),
+    url(r'^new$', views.new, name='new'),
+    url(r'^(?P<short>[a-z0-9]+)/d/$', views.shortdetails, name='shortdetails'),
+
 ]
--- links/shorts/views.py
+++ links/shorts/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.template import RequestContext, loader
+from django.utils import timezone
 
-from django.http import HttpResponse
-
+from .models import Link
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'short/index.html', {})
+
+def short(request, short):
+    link = get_object_or_404(Link, short=short)
+    link.clicks += 1
+    link.last_access_date = timezone.now()
+    link.save()
+    return render(request, 'short/go.html', {'link': link})
+
+def shortdetails(request, short):
+    link = get_object_or_404(Link, short=short)
+    return render(request, 'short/details.html', {'link': link})
+
+def new(request):
+    link = Link.create(url=request.POST['url'])
+    return render(request, 'short/details.html', {'link': link})
