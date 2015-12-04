3db4eb7d06443b4c18e2315f93b86a5fe2cdd6ce
Liudas Sodonis <liudas@sodonis.lt>
2015-12-03 21:07:01 -0800

    Added models; migration; registered to admin

--- links/shorts/models.py
+++ links/shorts/models.py
@@ -1,3 +1,32 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Link(models.Model):
+   url = models.CharField(max_length=2000)
+   short = models.CharField(max_length=10, db_index=True)
+   pub_date = models.DateTimeField('date published', auto_now_add=True, db_index=True)
+   last_access_date = models.DateTimeField('last accessed', auto_now_add=True, db_index=True)
+   clicks = models.IntegerField(default=0)
+
+   def was_published_recently(self):
+       return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+   def __str__(self):
+       return self.url + " (" + str(self.clicks) + ")";
+
+   @classmethod
+   def create(cls, url):
+       shortlistSet = ShortList.objects.filter(in_use=False)[:1]
+       for shortlist in shortlistSet:
+           short_url = shortlist.short
+           shortlist.in_use = True
+           shortlist.save()
+
+       link = cls(url=url, short=short_url, pub_date = timezone.now())
+       link.save()
+       return link
+
+class ShortList(models.Model):
+    short = models.CharField(max_length=10)
+    in_use = models.BooleanField(default=False)
