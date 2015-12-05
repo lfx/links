a51381c4eb778820cab9e142991afe3f5f01161c
Liudas Sodonis <liudas@sodonis.lt>
2015-12-05 15:13:09 -0800

    Added details view using generic views

--- links/shorts/urls.py
+++ links/shorts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<short>[a-z0-9]+)/$', views.short, name='short'),
     url(r'^new$', views.new, name='new'),
-    url(r'^(?P<short>[a-z0-9]+)/d/$', views.shortdetails, name='shortdetails'),
+    url(r'^(?P<short>[a-z0-9]+)/d/$', DetailView.as_view(model=Link,
+        queryset=Link.objects.filter(short=short)[0]), name='shortdetails'),
 ]
--- links/shorts/views.py
+++ links/shorts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext, loader
 from django.utils import timezone
+from django.views import generic
 
 from .models import Link
 
@@ -15,10 +16,6 @@ def short(request, short):
     link.last_access_date = timezone.now()
     link.save()
     return render(request, 'short/go.html', {'link': link})
-
-def shortdetails(request, short):
-    link = get_object_or_404(Link, short=short)
-    return render(request, 'short/details.html', {'link': link})
 
 def new(request):
     if 'url' in request.POST:
