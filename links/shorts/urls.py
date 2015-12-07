26048b0f904d89e683ed37fe4016168e02383758
Liudas Sodonis <liudas@sodonis.lt>
2015-12-06 16:15:45 -0800

    Rolled back changes for details view. Why it worked before?

--- links/shorts/urls.py
+++ links/shorts/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<short>[a-z0-9]+)/$', views.short, name='short'),
     url(r'^new$', views.new, name='new'),
-    url(r'^(?P<short>[a-z0-9]+)/d/$', DetailView.as_view(model=Link,
-        queryset=Link.objects.filter(short=short)[0]), name='shortdetails'),
+    url(r'^(?P<short>[a-z0-9]+)/d/$', views.details, name='shortdetails'),
 ]
--- links/shorts/views.py
+++ links/shorts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext, loader
 from django.utils import timezone
-from django.views import generic
 
 from .models import Link
 
@@ -22,3 +21,7 @@ def new(request):
         return render(request, 'short/details.html', {'link': link})
     else:
         return render(request, 'short/new.html', {})
+
+def details(request, short):
+    link = get_object_or_404(Link, short=short)
+    return render(request, 'short/details.html', {'link': link})
