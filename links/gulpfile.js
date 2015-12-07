f2b46df741d11571c303bf98b7b9eb380c8ad420
Liudas Sodonis <liudas@sodonis.lt>
2015-12-06 17:06:33 -0800

    Added zurb styles, adopted gulp.js and static file serving for development.

--- links/gulpfile.js
+++ links/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var $ = require('gulp-load-plugins')();
 
 gulp.task('jade', function() {
   var LOCALS = {};
@@ -13,4 +14,28 @@ gulp.task('jade', function() {
 
 gulp.task('watch', function() {
   gulp.watch('./shorts/templates/short_tpl/*.jade', ['jade']);
+  gulp.watch(['static_raw/zu/scss/**/*.scss'], ['sass']);
+  gulp.watch(['static_raw/zu/js/*.js'], ['js']);
 })
+
+var sassPaths = [
+  'static_raw/zu/bower_components/foundation-sites/scss',
+  'static_raw/zu/bower_components/motion-ui/src'
+];
+
+gulp.task('js', function() {
+  gulp.src('./static_raw/zu/js/app.js')
+    .pipe(gulp.dest('./static/style/js'));
+})
+
+gulp.task('sass', function() {
+  return gulp.src('static_raw/zu/scss/app.scss')
+    .pipe($.sass({
+        includePaths: sassPaths
+      })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('./static/style'));
+});
