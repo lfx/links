07111d16f6d22dd9dadfee624a642c32eee61e03
Liudas Sodonis <liudas@sodonis.lt>
2015-12-14 18:30:28 -0800

    Updateded url to be /

--- links/links/urls.py
+++ links/links/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^shorts/', include('shorts.urls')),
-]
+    url(r'', include('shorts.urls')),
+] + staticfiles_urlpatterns()
